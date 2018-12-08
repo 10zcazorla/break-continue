346edc16e6ddf4ca10f64c8b199daba44ece7be7
M. Zainur Ramdhan <45529730+10zcazorla@users.noreply.github.com>
2018-12-08 07:43:09 +0700

    Add files via upload
    #include <iostream>
    
    using namespace std;
    
    int main()
    {   for (int a=0; a<=10; a++){
        if (a==5){
            //break;
            continue;}
    
        cout <<a<<endl;
    }
        cout << "finish!" << endl;
        cin.get();
        return 0;
    }

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   for (int a=0; a<=10; a++){
+    if (a==5){
+        //break;
+        continue;}
+
+    cout <<a<<endl;
+}
+    cout << "finish!" << endl;
+    cin.get();
+    return 0;
+}
